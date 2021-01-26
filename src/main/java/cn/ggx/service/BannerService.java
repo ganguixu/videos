b570afdf98907d70eafd52849cf651df32c5be18
ganguixu <ganguixu1994@163.com>
2021-01-27 00:59:47 +0800

    换了个maven web的项目结构 BannerService.java

--- src/main/java/cn/ggx/service/BannerService.java
+++ src/main/java/cn/ggx/service/BannerService.java
@@ -1,4 +1,19 @@
 package cn.ggx.service;
 
+import cn.ggx.dao.BannerDao;
+import cn.ggx.entity.Banner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
 public class BannerService {
+    @Autowired
+    BannerDao bannerDao ;
+
+    public List<Banner> findBannerAll(){
+        List<Banner> bannerAll = bannerDao.findBannerAll();
+        return bannerAll ;
+    }
 }
